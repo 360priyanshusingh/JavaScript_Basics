b098079247e7c33836875669a74003b89cce817e
Priyanshu Sisodiya <upscpriyanshu06@gmail.com>
2024-09-03 17:10:33 +0530

    [Priyanshu] Added : Object_Prototypes

---
+++ Object_Prototypes.js
@@ -0,0 +1,19 @@
+// 1 Object Prototype :- The JavaScript prototype property allows you to add new properties to object constructors:
+// 1 example :-
+// function Person(first, last, age, eyecolor) {
+//     this.firstName = first;
+//     this.lastName = last;
+//     this.age = age;
+//     this.eyeColor = eyecolor;
+//   }
+//  Person.prototype.rol
+//  Person.prototype.name =function(){
+//     return this.firstName +" "+ this.lastName;
+//  };
+
+//  let object= new Person("priyanshu","sisodiya",21,"red");
+//  let object2= new Person("priyanshu22","sisodiya22",25,"red2");
+
+
+// console.log(object);  
+// console.log(object2.name());  

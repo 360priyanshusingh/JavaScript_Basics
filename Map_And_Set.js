8c986b602b90e30f6e107173d343bcdbe83f7e2a
Priyanshu Sisodiya <upscpriyanshu06@gmail.com>
2024-09-06 12:00:41 +0530

    [Priyanshu] Added : Map nad Set

---
+++ Map_And_Set.js
@@ -0,0 +1,35 @@
+let myMap =new Map()
+myMap.set("Priyanshu",1)
+myMap.set("Priyanshu",2)
+myMap.set("Sisodiya",2)
+myMap.set("Bhopal",3)
+myMap.set("Indor",4)
+myMap.set("Bhai esha mt kr yrr",4)
+// console.log(myMap);
+// console.log(myMap.has("Priyanshu"))
+// console.log(myMap.get('Sisodiya'))
+myMap.forEach((val,key)=>{
+    console.log(val,key);
+})
+// let itrator2 = myMap.entries()
+// for (const [key,val] of itrator2) {
+//     console.log(key,val)
+    
+// }
+
+let mySet= new Set();
+mySet.add(1)
+mySet.add(2)
+mySet.add(3)
+mySet.add(4)
+mySet.add(4)
+mySet.add(4)
+mySet.add(4)
+console.log(mySet);
+// let itrator=mySet.entries();
+// for(val of itrator){
+//     console.log(val)
+// }
+mySet.forEach((val)=>{
+    console.log(val)
+})
